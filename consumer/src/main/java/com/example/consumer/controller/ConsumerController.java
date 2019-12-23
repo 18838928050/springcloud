f034c557c614722174d52ea1ad8356b685629e33
fuhuifang <18838928050@163.com>
2019-12-23 23:01:25 +0800

    第一次提交

---
+++ consumer/src/main/java/com/example/consumer/controller/ConsumerController.java
@@ -0,0 +1,53 @@
+package com.example.consumer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class ConsumerController {
+    @Autowired
+    RestTemplate restTemplate;
+    @RequestMapping(value = "/ribbon-consumer",method = RequestMethod.GET)
+    public String helloController() {
+        return restTemplate.getForEntity("http://HELLO-SERVICE/hello", String.class).getBody();
+    }
+
+    @RequestMapping("/gethello")
+    public String getHello() {
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity("http://HELLO-SERVICE/hello", String.class);
+        String body = responseEntity.getBody();
+        HttpStatus statusCode = responseEntity.getStatusCode();
+        int statusCodeValue = responseEntity.getStatusCodeValue();
+        HttpHeaders headers = responseEntity.getHeaders();
+        StringBuffer result = new StringBuffer();
+        result.append("responseEntity.getBody()：").append(body).append("<hr>")
+                .append("responseEntity.getStatusCode()：").append(statusCode).append("<hr>")
+                .append("responseEntity.getStatusCodeValue()：").append(statusCodeValue).append("<hr>")
+                .append("responseEntity.getHeaders()：").append(headers).append("<hr>");
+        return result.toString();
+    }
+
+    @RequestMapping("/sayhello")
+    public String sayHello() {
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity("http://HELLO-SERVICE/sayhello?name={1}", String.class, "张三");
+        return responseEntity.getBody();
+    }
+
+    @RequestMapping("/sayhello2")
+    public String sayHello2() {
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "李四");
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity("http://HELLO-SERVICE/sayhello?name={name}", String.class, map);
+        return responseEntity.getBody();
+    }
+
+}
